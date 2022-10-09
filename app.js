bde3bed11a76a48fcf6cb6788023b8bb68eaa772
HyungJin <hyungjin0710@gmail.com>
2022-10-09 15:22:58 +0900

    #1.2 Paths

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
 // starting poing is a left and top corner.
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(350, 350, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
