94dd105c2c158a1fea8a51e1c8fcc855e9fed591
HyungJin <hyungjin0710@gmail.com>
2022-10-09 15:27:33 +0900

    #1.3 moveTo and lineTo

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// starting poing is a left and top corner.
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill();
-
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.fillStyle = "red";
+// starting poing is a left and top corner. (0, 0)
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.fill();
