0b12814c8ee39ab1ae66c520b2366f2cab9b5ba0
HyungJin <hyungjin0710@gmail.com>
2022-10-10 14:42:16 +0900

    #1.5 Drawing Project Two

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ canvas.width = 800;
 canvas.height = 800;
 
 // starting poing is a left and top corner. (0, 0)
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(200 - 60, 200 - 40, 15, 100);
+ctx.fillRect(285 - 60, 200 - 40, 50, 200);
+ctx.fillRect(400 - 60, 200 - 40, 15, 100);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(220 + 10, 75 + 5, 8, Math.PI, 2 * Math.PI);
+ctx.arc(260 + 10, 75 + 5, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
