2761c16696be96b93a10894ffe4303a558eaf6b3
HyungJin <hyungjin0710@gmail.com>
2022-10-10 19:25:55 +0900

    #2.4 Paint Color part Two

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
@@ -5,18 +8,6 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
-
-// const colors = [
-//   "#ff3838",
-//   "#ffb8b8",
-//   "#c56cf0",
-//   "#ff9f1a",
-//   "#fff200",
-//   "#32ff7e",
-//   "#7efff5",
-//   "#18dcff",
-//   "#7d5fff",
-// ];
 
 let isPainting = false;
 function onMove(event) {
@@ -41,6 +32,12 @@ function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
   ctx.fillStyle = event.target.value;
 }
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -50,3 +47,5 @@ canvas.addEventListener("mouseleave", cancelPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
 
 color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
