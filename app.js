df1e44f00571cbed4a7df0f01b0c7c3df044ff63
HyungJin <hyungjin0710@gmail.com>
2022-10-15 17:15:34 +0900

    #3.0 Adding Images

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
@@ -79,7 +80,19 @@ function onEraserClick() {
   modeBtn.innerText = "Fill";
 }
 
-canvas.addEventListener("mousemove", onMove);
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image(); // <img src="" />
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+    // So the user can replace the image if they want :)
+  };
+}
+
+canvas.addEventListener("mousemove", onMove); // canvas.onmousemove = onMove
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("click", onCanvasClick);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -94,3 +107,4 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
