eda02272b078aa317c52cce6c299cb7da5e83fc7
HyungJin <hyungjin0710@gmail.com>
2022-10-15 17:38:44 +0900

    #3.2 Saving Image

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const saveBtn = document.getElementById("save");
 const textInput = document.getElementById("text");
 const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
@@ -105,6 +106,15 @@ function onDoubleClick(event) {
   }
 }
 
+function onSaveClick() {
+  // <a href="" download
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+
 canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove); // canvas.onmousemove = onMove
 canvas.addEventListener("mousedown", startPainting);
@@ -122,3 +132,4 @@ modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
 fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
